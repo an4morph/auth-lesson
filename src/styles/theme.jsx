81f7fc8e65972921425b4da5ddada16d62bd74d2
an4morph <avgustin456@gmail.com>
2023-03-30 14:38:51 +0600

    remove unused style

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -3,8 +3,6 @@ import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './global'
 
 const PRIMARY_COLOR = '#6285DF'
-const PRIMARY_COLOR_HOVER = '#4e6fc2'
-const SECONDARY_COLOR = '#c0d0f8'
 
 const WHITE = '#FFFFFF'
 const BLACK = '#2C3343'
@@ -21,20 +19,6 @@ const theme = {
       bg: BLACK,
       text: WHITE,
     },
-    btn: {
-      primary: {
-        bg: PRIMARY_COLOR,
-        text: WHITE,
-        bgHover: PRIMARY_COLOR_HOVER,
-      },
-      secondary: {
-        bg: WHITE,
-        text: BLACK,
-        bgHover: SECONDARY_COLOR,
-        border: SECONDARY_COLOR,
-        borderHover: SECONDARY_COLOR,
-      }
-    }
   }
 }
 
