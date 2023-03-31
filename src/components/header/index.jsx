5cc39bcc182abad3d6710121f1ef29bedbb5d8c4
an4morph <avgustin456@gmail.com>
2023-03-31 09:22:37 +0600

    add logout

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 import { forwardRef } from "react"
+import { useNavigate } from "react-router-dom"
 import { Container } from "../../styles/adaptive"
 
 const HeaderWrapper = styled.header`
@@ -20,12 +21,19 @@ const LogoutButton = styled.button`
 `
 
 export const Header = forwardRef((props, ref) => {
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    localStorage.removeItem('token')
+    navigate('/auth/login')
+  }
+
   return (
     <HeaderWrapper ref={ref}>
       <Content>
         <div>Logo</div>
         
-        <LogoutButton>Log out</LogoutButton>
+        <LogoutButton onClick={handleLogout}>Log out</LogoutButton>
       </Content>
     </HeaderWrapper>
   )
